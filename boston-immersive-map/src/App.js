5d93df9e42cc0e5dfded382c39c50669802141cc
John <johnjo1116@gmail.com>
2025-01-25 13:14:50 -0500

    John + UI changed

--- boston-immersive-map/src/App.js
+++ boston-immersive-map/src/App.js
@@ -3,9 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import MapView from './components/MapView';
 import ImmersiveView from './components/ImmersiveView';
+import './App.css'; // Import CSS for the entire app
 
 function App() {
   return (
+    <div className="app-container">
+        {/* Project Info Box */}
+        <div className="project-info-box">
+        <h1 className="project-title">Boston TimeCapsule</h1>
+        <p className="project-subtitle">
+          Reconstructing Historic Objects with Nerfstudio
+        </p>
+      </div>
+
+
+    {/*Router for other pages*/}
     <Router>
       <Routes>
         {/* Main Map Page */}
@@ -16,6 +28,7 @@ function App() {
         <Route path="/immersive/:siteId" element={<ImmersiveView />} />
       </Routes>
     </Router>
+    </div>
   );
 }
 
